020cc62012b7d50eb04bac9622b9d5c24b458772
red3n <valiullinartur1@gmail.com>
2025-12-18 03:51:35 +0400

    Add files via upload

---
+++ lab05/lab05/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace lab05
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnColor = new System.Windows.Forms.Button();
+            this.btnFont = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButtonZoom = new System.Windows.Forms.RadioButton();
+            this.radioButtonStretch = new System.Windows.Forms.RadioButton();
+            this.radioButtonNormal = new System.Windows.Forms.RadioButton();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.panelDraw = new System.Windows.Forms.Panel();
+            this.pictureBoxPixels = new System.Windows.Forms.PictureBox();
+            this.btnInvert = new System.Windows.Forms.Button();
+            this.btnLoadPixels = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPixels)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnColor
+            // 
+            this.btnColor.Location = new System.Drawing.Point(55, 284);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(75, 23);
+            this.btnColor.TabIndex = 0;
+            this.btnColor.Text = "Цвет текста";
+            this.btnColor.UseVisualStyleBackColor = true;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // btnFont
+            // 
+            this.btnFont.Location = new System.Drawing.Point(55, 313);
+            this.btnFont.Name = "btnFont";
+            this.btnFont.Size = new System.Drawing.Size(75, 23);
+            this.btnFont.TabIndex = 1;
+            this.btnFont.Text = "Шрифт";
+            this.btnFont.UseVisualStyleBackColor = true;
+            this.btnFont.Click += new System.EventHandler(this.btnFont_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(392, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(186, 150);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButtonZoom);
+            this.panel1.Controls.Add(this.radioButtonStretch);
+            this.panel1.Controls.Add(this.radioButtonNormal);
+            this.panel1.Location = new System.Drawing.Point(392, 187);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(186, 100);
+            this.panel1.TabIndex = 3;
+            // 
+            // radioButtonZoom
+            // 
+            this.radioButtonZoom.AutoSize = true;
+            this.radioButtonZoom.Location = new System.Drawing.Point(23, 65);
+            this.radioButtonZoom.Name = "radioButtonZoom";
+            this.radioButtonZoom.Size = new System.Drawing.Size(52, 17);
+            this.radioButtonZoom.TabIndex = 2;
+            this.radioButtonZoom.TabStop = true;
+            this.radioButtonZoom.Text = "Zoom";
+            this.radioButtonZoom.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonStretch
+            // 
+            this.radioButtonStretch.AutoSize = true;
+            this.radioButtonStretch.Location = new System.Drawing.Point(23, 41);
+            this.radioButtonStretch.Name = "radioButtonStretch";
+            this.radioButtonStretch.Size = new System.Drawing.Size(59, 17);
+            this.radioButtonStretch.TabIndex = 1;
+            this.radioButtonStretch.TabStop = true;
+            this.radioButtonStretch.Text = "Stretch";
+            this.radioButtonStretch.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonNormal
+            // 
+            this.radioButtonNormal.AutoSize = true;
+            this.radioButtonNormal.Location = new System.Drawing.Point(23, 17);
+            this.radioButtonNormal.Name = "radioButtonNormal";
+            this.radioButtonNormal.Size = new System.Drawing.Size(58, 17);
+            this.radioButtonNormal.TabIndex = 0;
+            this.radioButtonNormal.TabStop = true;
+            this.radioButtonNormal.Text = "Normal";
+            this.radioButtonNormal.UseVisualStyleBackColor = true;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(621, 76);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 4;
+            this.btnLoad.Text = "Загрузить";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // panelDraw
+            // 
+            this.panelDraw.Location = new System.Drawing.Point(597, 145);
+            this.panelDraw.Name = "panelDraw";
+            this.panelDraw.Size = new System.Drawing.Size(200, 100);
+            this.panelDraw.TabIndex = 5;
+            this.panelDraw.Paint += new System.Windows.Forms.PaintEventHandler(this.panelDraw_Paint);
+            // 
+            // pictureBoxPixels
+            // 
+            this.pictureBoxPixels.Location = new System.Drawing.Point(230, 333);
+            this.pictureBoxPixels.Name = "pictureBoxPixels";
+            this.pictureBoxPixels.Size = new System.Drawing.Size(156, 105);
+            this.pictureBoxPixels.TabIndex = 6;
+            this.pictureBoxPixels.TabStop = false;
+            // 
+            // btnInvert
+            // 
+            this.btnInvert.Location = new System.Drawing.Point(392, 344);
+            this.btnInvert.Name = "btnInvert";
+            this.btnInvert.Size = new System.Drawing.Size(105, 23);
+            this.btnInvert.TabIndex = 7;
+            this.btnInvert.Text = "Инвертировать";
+            this.btnInvert.UseVisualStyleBackColor = true;
+            this.btnInvert.Click += new System.EventHandler(this.btnInvert_Click);
+            // 
+            // btnLoadPixels
+            // 
+            this.btnLoadPixels.Location = new System.Drawing.Point(392, 373);
+            this.btnLoadPixels.Name = "btnLoadPixels";
+            this.btnLoadPixels.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadPixels.TabIndex = 8;
+            this.btnLoadPixels.Text = "Загрузить";
+            this.btnLoadPixels.UseVisualStyleBackColor = true;
+            this.btnLoadPixels.Click += new System.EventHandler(this.btnLoadPixels_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLoadPixels);
+            this.Controls.Add(this.btnInvert);
+            this.Controls.Add(this.pictureBoxPixels);
+            this.Controls.Add(this.panelDraw);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnFont);
+            this.Controls.Add(this.btnColor);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPixels)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.Button btnFont;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButtonZoom;
+        private System.Windows.Forms.RadioButton radioButtonStretch;
+        private System.Windows.Forms.RadioButton radioButtonNormal;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Panel panelDraw;
+        private System.Windows.Forms.PictureBox pictureBoxPixels;
+        private System.Windows.Forms.Button btnInvert;
+        private System.Windows.Forms.Button btnLoadPixels;
+    }
+}
+
---
+++ lab05/lab05/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab05
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            using (LinearGradientBrush brush =
+        new LinearGradientBrush(
+            new Rectangle(20, 20, 150, 100),
+            Color.LightBlue,
+            Color.DarkBlue,
+            45))
+            {
+                g.FillEllipse(brush, 20, 20, 150, 100);
+            }
+
+            // ===== 2. НАБОР ЛИНИЙ + EndCap =====
+            using (Pen pen = new Pen(Color.Red, 3))
+            {
+                pen.EndCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor;
+
+                g.DrawLine(pen, 200, 30, 350, 30);
+                g.DrawLine(pen, 200, 60, 350, 80);
+                g.DrawLine(pen, 200, 100, 350, 130);
+            }
+
+            // ===== 3. КРИВЫЕ БЕЗЬЕ =====
+            using (Pen bezierPen = new Pen(Color.Green, 2))
+            {
+                Point p1 = new Point(20, 150);
+                Point p2 = new Point(100, 200);
+                Point p3 = new Point(150, 100);
+                Point p4 = new Point(250, 180);
+
+                g.DrawBezier(bezierPen, p1, p2, p3, p4);
+            }
+
+            // ===== 4. ТЕКСТ =====
+            using (Font font = new Font("Arial", 14, FontStyle.Bold))
+            using (Brush textBrush = new SolidBrush(Color.Purple))
+            {
+                g.DrawString("Графика Windows Forms", font, textBrush, 20, 250);
+            }
+
+            GraphicsPath path = new GraphicsPath();
+
+            int x = figurePosition.X;
+            int y = figurePosition.Y;
+
+            // Стрелка (сложная фигура)
+            path.AddLine(x, y, x + 60, y);
+            path.AddLine(x + 60, y, x + 60, y - 20);
+            path.AddLine(x + 100, y + 20, x + 60, y + 60);
+            path.AddLine(x + 60, y + 60, x + 60, y + 40);
+            path.AddLine(x, y + 40, x, y);
+            path.CloseFigure();
+
+            // Заливка
+            g.FillPath(Brushes.Orange, path);
+            g.DrawPath(Pens.Black, path);
+        }
+        Color textColor = Color.Purple;
+        Font textFont = new Font("Arial", 14, FontStyle.Bold);
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog dlg = new ColorDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                textColor = dlg.Color;
+                Invalidate(); // перерисовка
+            }
+        }
+
+        private void btnFont_Click(object sender, EventArgs e)
+        {
+            FontDialog dlg = new FontDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                textFont = dlg.Font;
+                Invalidate();
+            }
+        }
+        private void radioButtonNormal_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+        }
+
+        private void radioButtonStretch_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void radioButtonZoom_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Images|*.jpg;*.png;*.bmp";
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(dlg.FileName);
+            }
+        }
+
+        private void panelDraw_Paint(object sender, PaintEventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                e.Graphics.DrawImage(pictureBox1.Image, 10, 10, 100, 100);
+            }
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            figurePosition = e.Location;
+            Invalidate(); // перерисовать форму
+        }
+        Point figurePosition = new Point(150, 150);
+
+        Bitmap bmp;
+
+        private void btnLoadPixels_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Images|*.bmp;*.jpg;*.png";
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                bmp = new Bitmap(dlg.FileName);
+                pictureBoxPixels.Image = bmp;
+            }
+        }
+
+        private void btnInvert_Click(object sender, EventArgs e)
+        {
+            if (bmp == null) return;
+
+            for (int y = 0; y < bmp.Height; y++)
+            {
+                for (int x = 0; x < bmp.Width; x++)
+                {
+                    Color c = bmp.GetPixel(x, y);
+                    Color inverted = Color.FromArgb(
+                        255 - c.R,
+                        255 - c.G,
+                        255 - c.B
+                    );
+
+                    bmp.SetPixel(x, y, inverted);
+                }
+            }
+
+            pictureBoxPixels.Invalidate();
+        }
+    }
+}
